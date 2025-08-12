c5e669ace3ab7ef9df62cc28d98d4246df9b0119
CarlosBorges <Carloseduana2004@gmail.com>
2025-08-12 20:36:53 -0300

    Inserindo nome e salário

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,8 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-int idade = 24;
-Console.WriteLine("A idade é" + idade);
-if (idade >= 18)
-    Console.WriteLine("Você pode tirar habilitação");
-// quando o if tem uma unica linha, n precisa de separador de bloco ({})
-else
-    Console.WriteLine("Você não pode tirar a habilitação");
+﻿Console.WriteLine("Informe seu nome: ");
+string nome = Console.ReadLine();
+Console.WriteLine("Informe seu salário: ");
+decimal salario = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Seja bem-vindo(a) {0}, o salário informado foi ${1}", nome, salario);
