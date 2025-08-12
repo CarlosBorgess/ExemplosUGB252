1bf02e77eee7e4442103466affa7f4cc99de67f4
CarlosBorges <Carloseduana2004@gmail.com>
2025-08-12 19:51:32 -0300

    Acrescentado o else

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -2,6 +2,7 @@
 int idade = 24;
 Console.WriteLine("A idade é" + idade);
 if (idade >= 18)
-{
     Console.WriteLine("Você pode tirar habilitação");
-}
+// quando o if tem uma unica linha, n precisa de separador de bloco ({})
+else
+    Console.WriteLine("Você não pode tirar a habilitação");
